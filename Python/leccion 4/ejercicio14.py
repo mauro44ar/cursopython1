9d203b1a5d6d6c8c4e86abddf2b3b15283628143
mauro44ar <mauro43ar@gmmail.com>
2024-08-01 12:28:05 +0100

    actualizando el repositorio nuevamente 1-08-2024

---
+++ Python/leccion 4/ejercicio14.py
@@ -0,0 +1,56 @@
+#suma
+operando1=int(input('ingrese valor : '))
+
+operando2 =int(input('ingrese valor2: '))
+
+suma = operando1 + operando2
+
+#print ('el resultado de la suma de ',operando1 ,' y de ', operando2, 'es : ',suma)
+
+print(f'el resultado de la suma de {operando1} y de {operando2} es: {suma}') #esto es una interpolacion 
+
+#resta
+
+operando3=int(input('ingrese valor 3: '))
+
+operando4=int(input('ingrese el valor 4: '))
+              
+resta= operando3 - operando4
+
+print(f'la resta del valor {operando3} y el valor {operando4} es: {resta}')
+
+#multiplicacion
+
+operando5= int(input('ingrese el valor 5: '))
+
+operando6=int(input('ingrese el valor 6: '))
+
+multiplicacion= operando5 * operando6
+
+print (f'el resultado de la multiplicacion de los valores {operando5} por {operando6} es : {multiplicacion}')
+
+#division
+
+operando7=int(input('ingrese valor 7 : '))
+
+operando8 =int(input('ingrese valor 8: '))
+
+division= operando7 / operando8
+
+print(f'la divicion de los valores {operando7} por {operando8} es: {division}')
+
+#division entera
+division= operando7 // operando8
+
+print(f'la divicion de los valores {operando7} por {operando8} es: {division}')
+
+#modulo
+
+modulo= operando7 % operando8
+
+print(f'resultado del residuo de los valores {operando7} por {operando8} es: {modulo}')
+
+#potencia
+potencia= operando7 * operando8
+
+print(f'la potencia del valor {operando7} por {operando8} es: {potencia}')
