47fff67549231c6a43b1807457fe45dc21454e58
mauro44ar <mauro43ar@gmmail.com>
2024-08-10 12:24:14 +0100

    actualizando 10-08

---
+++ Python/leccion 5/If-Else.py
@@ -0,0 +1,9 @@
+condicion = False
+
+if condicion == True:
+    print ('condicion verdadera')
+elif condicion == False:
+    print('condicion falsa')
+else:
+    print('condicion no reconocida')
+    
